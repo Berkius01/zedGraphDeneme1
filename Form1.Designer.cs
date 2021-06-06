47108506d5bf2e5c73c1820d34dfa4cb05e3a496
BERK SAGLICAK <21895855@ogr.baskent.edu.tr>
2021-06-06 15:55:53 +0300

    model uydu deneme

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace zedGraphDeneme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.portlar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl5 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl4 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl6 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(23, 206);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(58, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // portlar
+            // 
+            this.portlar.FormattingEnabled = true;
+            this.portlar.Location = new System.Drawing.Point(274, 52);
+            this.portlar.Name = "portlar";
+            this.portlar.Size = new System.Drawing.Size(121, 24);
+            this.portlar.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "br";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(274, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 31);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "bağlantıyı kes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(482, 206);
+            this.zedGraphControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl2.TabIndex = 12;
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.Location = new System.Drawing.Point(482, 591);
+            this.zedGraphControl3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.ScrollGrace = 0D;
+            this.zedGraphControl3.ScrollMaxX = 0D;
+            this.zedGraphControl3.ScrollMaxY = 0D;
+            this.zedGraphControl3.ScrollMaxY2 = 0D;
+            this.zedGraphControl3.ScrollMinX = 0D;
+            this.zedGraphControl3.ScrollMinY = 0D;
+            this.zedGraphControl3.ScrollMinY2 = 0D;
+            this.zedGraphControl3.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl3.TabIndex = 13;
+            // 
+            // zedGraphControl5
+            // 
+            this.zedGraphControl5.Location = new System.Drawing.Point(23, 591);
+            this.zedGraphControl5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl5.Name = "zedGraphControl5";
+            this.zedGraphControl5.ScrollGrace = 0D;
+            this.zedGraphControl5.ScrollMaxX = 0D;
+            this.zedGraphControl5.ScrollMaxY = 0D;
+            this.zedGraphControl5.ScrollMaxY2 = 0D;
+            this.zedGraphControl5.ScrollMinX = 0D;
+            this.zedGraphControl5.ScrollMinY = 0D;
+            this.zedGraphControl5.ScrollMinY2 = 0D;
+            this.zedGraphControl5.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl5.TabIndex = 14;
+            // 
+            // zedGraphControl4
+            // 
+            this.zedGraphControl4.Location = new System.Drawing.Point(940, 591);
+            this.zedGraphControl4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl4.Name = "zedGraphControl4";
+            this.zedGraphControl4.ScrollGrace = 0D;
+            this.zedGraphControl4.ScrollMaxX = 0D;
+            this.zedGraphControl4.ScrollMaxY = 0D;
+            this.zedGraphControl4.ScrollMaxY2 = 0D;
+            this.zedGraphControl4.ScrollMinX = 0D;
+            this.zedGraphControl4.ScrollMinY = 0D;
+            this.zedGraphControl4.ScrollMinY2 = 0D;
+            this.zedGraphControl4.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl4.TabIndex = 15;
+            // 
+            // zedGraphControl6
+            // 
+            this.zedGraphControl6.Location = new System.Drawing.Point(940, 206);
+            this.zedGraphControl6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl6.Name = "zedGraphControl6";
+            this.zedGraphControl6.ScrollGrace = 0D;
+            this.zedGraphControl6.ScrollMaxX = 0D;
+            this.zedGraphControl6.ScrollMaxY = 0D;
+            this.zedGraphControl6.ScrollMaxY2 = 0D;
+            this.zedGraphControl6.ScrollMinX = 0D;
+            this.zedGraphControl6.ScrollMinY = 0D;
+            this.zedGraphControl6.ScrollMinY2 = 0D;
+            this.zedGraphControl6.Size = new System.Drawing.Size(438, 363);
+            this.zedGraphControl6.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1525, 986);
+            this.Controls.Add(this.zedGraphControl6);
+            this.Controls.Add(this.zedGraphControl4);
+            this.Controls.Add(this.zedGraphControl5);
+            this.Controls.Add(this.zedGraphControl3);
+            this.Controls.Add(this.zedGraphControl2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portlar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox portlar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private ZedGraph.ZedGraphControl zedGraphControl3;
+        private ZedGraph.ZedGraphControl zedGraphControl5;
+        private ZedGraph.ZedGraphControl zedGraphControl4;
+        private ZedGraph.ZedGraphControl zedGraphControl6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using System.IO;
+using System.IO.Ports;
+
+namespace zedGraphDeneme1
+{
+    public partial class Form1 : Form
+    {
+        GraphPane yükseklik = new GraphPane();
+        GraphPane basinc = new GraphPane();
+        GraphPane sicaklik = new GraphPane();
+        GraphPane x = new GraphPane();
+        GraphPane y = new GraphPane();
+        GraphPane z = new GraphPane();
+
+
+        PointPairList pointYükseklik = new PointPairList();
+        PointPairList pointBasinc = new PointPairList();
+        PointPairList pointSicaklik = new PointPairList();
+        PointPairList pointX = new PointPairList();
+        PointPairList pointY = new PointPairList();
+        PointPairList pointZ = new PointPairList();
+
+        LineItem curveYükseklik;
+        LineItem curveBasinc;
+        LineItem curveSicaklik;
+        LineItem curveX;
+        LineItem curveY;
+        LineItem curveZ;
+
+        double zaman;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            comboBox1.Items.Add(300);
+            comboBox1.Items.Add(1200);
+            comboBox1.Items.Add(2400);
+            comboBox1.Items.Add(9600);
+            comboBox1.Items.Add(19200);
+            comboBox1.Items.Add(57600);
+
+            portlar.Items.Add("COM1");
+            portlar.Items.Add("COM2");
+            portlar.Items.Add("COM3");
+            portlar.Items.Add("COM4");
+            portlar.Items.Add("COM5");
+            portlar.Items.Add("COM6");
+
+            grafik();
+        }
+        private void grafik()
+        {
+            yükseklik = zedGraphControl1.GraphPane;
+            yükseklik.Title.Text = "yükseklik - zaman grafiği";
+            yükseklik.XAxis.Title.Text = "t(s)";
+            yükseklik.YAxis.Title.Text = "H(m)";
+            yükseklik.YAxis.Scale.Min = 0;
+            curveYükseklik = yükseklik.AddCurve(null, pointYükseklik, Color.Red, SymbolType.None);
+            curveYükseklik.Line.Width = 1;
+            // yükseklik için
+
+            basinc = zedGraphControl2.GraphPane;
+            basinc.Title.Text = "basınç - zaman grafiği";
+            basinc.XAxis.Title.Text = "t(s)";
+            basinc.YAxis.Title.Text = "P(Pa)";
+            basinc.YAxis.Scale.Min = 0;
+            curveBasinc = basinc.AddCurve(null, pointBasinc, Color.Red, SymbolType.None);
+            curveBasinc.Line.Width = 1;
+            //basınç
+
+            sicaklik = zedGraphControl3.GraphPane;
+            sicaklik.Title.Text = "sıcaklık - zaman grafiği";
+            sicaklik.XAxis.Title.Text = "t(s)";
+            sicaklik.YAxis.Title.Text = "T(C)";
+            sicaklik.YAxis.Scale.Min = 0;
+            curveSicaklik = sicaklik.AddCurve(null, pointSicaklik, Color.Red, SymbolType.None);
+            curveSicaklik.Line.Width = 1;
+            //sıcaklık
+
+            x = zedGraphControl4.GraphPane;
+            x.Title.Text = "X - zaman grafiği";
+            x.XAxis.Title.Text = "t(s)";
+            x.YAxis.Title.Text = "x";
+            x.YAxis.Scale.Min = 0;
+            curveX = x.AddCurve(null, pointX, Color.Red, SymbolType.None);
+            curveX.Line.Width = 1;
+            //x
+
+            y = zedGraphControl5.GraphPane;
+            y.Title.Text = "y - zaman grafiği";
+            y.XAxis.Title.Text = "t(s)";
+            y.YAxis.Title.Text = "y";
+            y.YAxis.Scale.Min = 0;
+            curveY = y.AddCurve(null, pointY, Color.Red, SymbolType.None);
+            curveY.Line.Width = 1;
+            //y
+
+            z = zedGraphControl6.GraphPane;
+            z.Title.Text = "z - zaman grafiği";
+            z.XAxis.Title.Text = "t(s)";
+            z.YAxis.Title.Text = "z";
+            z.YAxis.Scale.Min = 0;
+            curveZ = z.AddCurve(null, pointZ, Color.Red, SymbolType.None);
+            curveZ.Line.Width = 1;
+            //z
+
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string[] data = serialPort1.ReadLine().Split(';');
+            zaman += 0.05;
+            pointYükseklik.Add(new PointPair(zaman, Convert.ToDouble(data[1].ToString())));
+            pointX.Add(new PointPair(zaman, Convert.ToDouble(data[2].ToString())));
+            pointY.Add(new PointPair(zaman, Convert.ToDouble(data[3].ToString())));
+            pointZ.Add(new PointPair(zaman, Convert.ToDouble(data[4].ToString())));
+            pointSicaklik.Add(new PointPair(zaman, Convert.ToDouble(data[5].ToString())));
+            pointBasinc.Add(new PointPair(zaman, Convert.ToDouble(data[6].ToString())));
+
+            yükseklik.XAxis.Scale.Max = zaman;
+            x.XAxis.Scale.Max = zaman;
+            y.XAxis.Scale.Max = zaman;
+            z.XAxis.Scale.Max = zaman;
+            sicaklik.XAxis.Scale.Max = zaman;
+            basinc.XAxis.Scale.Max = zaman;
+
+            yükseklik.AxisChange();
+            x.AxisChange();
+            y.AxisChange();
+            z.AxisChange();
+            sicaklik.AxisChange();
+            basinc.AxisChange();
+
+            zedGraphControl1.Refresh();
+            zedGraphControl2.Refresh();
+            zedGraphControl3.Refresh();
+            zedGraphControl4.Refresh();
+            zedGraphControl5.Refresh();
+            zedGraphControl6.Refresh();
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" && portlar.Text == "")
+            {
+                MessageBox.Show("port ve baudrate seç");
+            }
+            else {
+                serialPort1.PortName = portlar.SelectedItem.ToString();
+                serialPort1.BaudRate = int.Parse(comboBox1.SelectedItem.ToString());
+                serialPort1.Open();
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.Handshake = Handshake.None;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+    }
+}
